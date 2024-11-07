ec8523ec98b0321ce5eea9ef731e3d0d3cb2d538
Adam Zieliński <adam.zie231@gmail.com>
2024-11-07 14:23:50 +0100

    init java + springBoot project

---
+++ backend/src/test/java/makonet/MakonetApplicationTests.java
@@ -0,0 +1,13 @@
+package makonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
