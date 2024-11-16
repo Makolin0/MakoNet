ea939d4fc1cfb400122383adc1fe95a5d169fcd2
Adam Zieliński <adam.zie231@gmail.com>
2024-11-17 00:24:42 +0100

    working connection with mongodb database

--- backend/src/main/java/com/makonet/controllers/UserController.java
+++ backend/src/main/java/com/makonet/controllers/UserController.java
@@ -1,33 +1,29 @@
 package com.makonet.controllers;
 
-import com.makonet.MyUser;
+import com.makonet.models.MongoUser;
+import com.makonet.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
-    List<MyUser> myUsers = new ArrayList<>(List.of(
-            new MyUser(1, "Adam", "123"),
-            new MyUser(2, "Kuba", "456"),
-            new MyUser(3, "Adrian", "789")
-    ));
+    @Autowired
+    UserRepository userRepo;
 
     @GetMapping("users")
-    public List<MyUser> getUsers() {
-        return myUsers;
+    public List<MongoUser> getUsers() {
+        return userRepo.findAll();
     }
 
-    @PostMapping("users")
-    public void addUser(@RequestBody MyUser myUser) {
-        myUsers.add(myUser);
-    }
+//    @PostMapping("users")
+//    public void addUser(@RequestBody MyUser myUser) {
+//        myUsers.add(myUser);
+//    }
 
     @GetMapping("csrf")
     public CsrfToken getCsrfToken(HttpServletRequest request) {
---
+++ backend/src/main/java/com/makonet/models/MongoUser.java
@@ -0,0 +1,43 @@
+package com.makonet.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+@ToString
+public class MongoUser {
+    private String nickname;
+    private String email;
+    private String password;
+    private String registrationTime;
+    private String[] roles;
+    private Lootboxes lootboxes;
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+class Lootboxes {
+    private Map<String, Integer> unopened;
+    private History[] history;
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+class History {
+    private String rewardId;
+    private LocalDateTime dropTime;
+    private LocalDateTime receivedTime;
+}
