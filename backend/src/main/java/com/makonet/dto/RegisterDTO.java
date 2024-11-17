4db35b22949382ccec7890b1040ed109c3663ddb
Adam Zieliński <adam.zie231@gmail.com>
2024-11-17 22:23:55 +0100

    add Login and Register DTO

---
+++ backend/src/main/java/com/makonet/dto/RegisterDTO.java
@@ -0,0 +1,15 @@
+package com.makonet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterDTO {
+    private String username;
+    private String email;
+    private String password;
+    private String confirmPassword;
+}
--- backend/src/main/java/com/makonet/services/UserService.java
+++ backend/src/main/java/com/makonet/services/UserService.java
@@ -1,9 +1,14 @@
 package com.makonet.services;
 
+import com.makonet.dto.LoginDTO;
+import com.makonet.dto.RegisterDTO;
 import com.makonet.models.Lootboxes;
 import com.makonet.models.MongoUser;
 import com.makonet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,13 +21,37 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepository userRepo;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
+
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
-    public MongoUser saveUser(MongoUser user) {
-        user.setPassword(encoder.encode(user.getPassword()));
+    public MongoUser saveUser(RegisterDTO register) {
+        if(!register.getPassword().equals(register.getConfirmPassword())) {
+            return null;
+        }
+
+        MongoUser user = new MongoUser();
+
+        user.setUsername(register.getUsername());
+        user.setEmail(register.getEmail());
+        user.setPassword(encoder.encode(register.getPassword()));
         user.setRegistrationTime(LocalDateTime.now());
         user.setRoles(new ArrayList<>(List.of(new SimpleGrantedAuthority("ROLE_USER"))));
         user.setLootboxes(new Lootboxes());
+
         return userRepo.save(user);
+    }
+
+    public String generateJwt(LoginDTO credentials) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(credentials.getUsername(), credentials.getPassword()));
+
+        if(authentication.isAuthenticated()) {
+            return jwtService.generateToken(credentials.getUsername());
+        } else {
+            return "Login failed";
+        }
     }
 }
